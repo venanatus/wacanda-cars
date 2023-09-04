17eb5a935c1a6e0d350ce15a342d02fe7b2b5bc2
Tim <102161572+tihero10062002@users.noreply.github.com>
2023-09-04 20:44:53 +0500

    Delete users directory

--- users/bulma_mixin.py
+++
@@ -1,8 +0,0 @@
-class BulmaMixin(object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            if visible.field.widget.input_type in ('text', 'email', 'password', 'number', 'url'):
-                visible.field.widget.attrs['class'] = 'input'
-            elif visible.field.widget.input_type == 'file':
-                visible.field.widget.template_name = 'file_input.html'
